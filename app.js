13c18beba84401520b2bb93f893e710f0e862bb3
Tolulope Fakunle <fakunletolulope05@gmail.com>
2022-12-30 18:36:07 +0100

    playing with nodejs

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-const fs = require("fs")
+const fs = require("node:fs")
+const https = require('node:https');
 
 fs.writeFileSync("hello.txt","Tolu: 'How are you?'")
 fs.appendFileSync("hello.txt","Seyi: 'I am fine too and you?'")
+
+
+https.get('https://encrypted.google.com/', (res) => {
+  console.log('statusCode:', res.statusCode);
+  console.log('headers:', res.headers);
+
+  res.on('data', (d) => {
+    process.stdout.write(d);
+  });
+
+}).on('error', (e) => {
+  console.error(e);
+});

f9c63fee6174ea1012a3190babea894c1f8752b7
Tolulope Fakunle <fakunletolulope05@gmail.com>
2022-12-31 02:16:33 +0100

    we learnt geting input from user

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const validator = require("validator")
-const note = require("./utils.js");
 
-console.log(validator.equals("Tolu","tolu"))
+const getNotes = require("./note.js");
+
+// console.log(process.argv[2])
+
+if (process.argv[2]==="add") {
+    console.log("Ading New Note")
+} else if (process.argv[2]==="remove"){
+console.log("Deleting your note!")
+}
---
+++ note.js
@@ -0,0 +1,11 @@
+// const fs = require("node:fs");
+
+
+// fs.writeFileSync("hello.txt","Tolu: 'How are you?'")
+// fs.appendFileSync("hello.txt","Seyi: 'I am fine too and you?'")
+
+const note= function(){
+return "Your Note is doing fine..."
+}
+
+module.exports = note;

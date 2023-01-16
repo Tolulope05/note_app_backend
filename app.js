9a6aea64d943bf437b22d18eba1465182f19014c
Tolulope Fakunle <fakunletolulope05@gmail.com>
2023-01-16 17:16:05 +0100

    did boss hendrix asignment

--- app.js
+++ app.js
@@ -8,3 +8,16 @@ const yargs = require("yargs")
 console.log(yargs.argv)
 
 
+
+// const fs = require("node:fs");
+
+// for (let index = 0; index < 1000; index++) {
+//     if (index = 1) {
+//         fs.writeFileSync("filename.txt","file you want to copy")
+//     } else {
+//     fs.appendFileSync("filename.txt","\n file you want to copy")
+//     }
+    
+// }
+
+

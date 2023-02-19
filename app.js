a13dc2fc569c0d57c937eb761ea27dd06b69c22a
Tolulope Fakunle <fakunletolulope05@gmail.com>
2023-02-19 20:28:54 +0100

    clean up

--- app.js
+++ app.js
@@ -6,18 +6,3 @@ const yargs = require("yargs")
 
 // console.log(process.argv)
 console.log(yargs.argv)
-
-
-
-// const fs = require("node:fs");
-
-// for (let index = 0; index < 1000; index++) {
-//     if (index = 1) {
-//         fs.writeFileSync("filename.txt","file you want to copy")
-//     } else {
-//     fs.appendFileSync("filename.txt","\n file you want to copy")
-//     }
-    
-// }
-
-

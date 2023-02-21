5244bd5025330edf06bbe37d735fc10690536cc2
Tolulope Fakunle <fakunletolulope05@gmail.com>
2023-02-21 14:50:20 +0100

    updating file system

--- playground/1-json.js
+++ playground/1-json.js
@@ -12,7 +12,18 @@ const fs = require("fs");
 // fs.writeFileSync("1-json.json", bookJson);
 
 // want to read the file created
-const dataBuffer = fs.readFileSync("1-json.json"); //i.e its binary data
-const dataJson = dataBuffer.toString(); // standard strinf in js
-const data = JSON.parse(dataJson); //parsed data
-console.log(data.author);
+// const dataBuffer = fs.readFileSync("1-json.json"); //i.e its binary data
+// const dataJson = dataBuffer.toString(); // standard strinf in js
+// const data = JSON.parse(dataJson); //parsed data
+// console.log(data.author);
+
+/**NEW Challenge */
+const dataBuffer = fs.readFileSync("1-json.json");
+const dataJon = dataBuffer.toString();
+
+dataParsed = JSON.parse(dataJon);
+
+dataParsed.name = "Timileyin";
+
+newData = JSON.stringify(dataParsed);
+fs.writeFileSync("1-json.json", newData);
